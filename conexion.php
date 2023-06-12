7ef7addcbc926851cc5d22b557a5bd14c9dba636
DFernandez <83619210+dfernandeztese@users.noreply.github.com>
2023-06-11 18:45:55 -0600

    Primer Avance Del Proyecto

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+$conexion = mysqli_connect('localhost', 'root', '', 'ecojoba_db') or die('Error en la conexion servidor');
+
+$sql = "INSERT INTO usuario 
+VALUES(null,'".$_POST["nombreCompleto"]."','".$_POST["correoElectronico"]."','".$_POST["nombreUsuario"]."','".$_POST["contrasenia"]."')";
+
+$res = mysqli_query($conexion,$sql) or die("Error Al Momento De Almacenar La informacion En La Base De Datos");
+
+mysqli_close($conexion);
+
+echo 'Datos Ingresador Por El Usuario:'.'<br>';
+echo 'Nombre Completo: ' .$_POST["nombreCompleto"].'<br>';
+echo 'Correo Electronico: ' .$_POST["correoElectronico"].'<br>';
+echo 'Nombre DE Usuario: ' .$_POST["correoElectronico"].'<br>';
+?>
